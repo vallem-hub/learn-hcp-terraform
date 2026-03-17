33fd1fb74e08b12993fdaf0274b39f204cccc185
vallem-hub <vallem.pspk@gmail.com>
2026-03-17 22:46:15 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -11,7 +11,7 @@ output "instance_subnet" {
   value = aws_instance.app_server.subnet_id
 }
 
-+ output "instance_ami" {
-+   description = "AMI used to create the EC2 instance."
-+   value       = aws_instance.app_server.ami
-+ }
+ output "instance_ami" {
+   description = "AMI used to create the EC2 instance."
+   value       = aws_instance.app_server.ami
+ }
