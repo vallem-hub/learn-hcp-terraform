3a502a2fdb3d6eab28cb040a7a3a86eebc310f49
vallem-hub <vallem.pspk@gmail.com>
2026-03-17 22:35:20 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -10,3 +10,20 @@ output "instance_security_group_ids" {
 output "instance_subnet" {
   value = aws_instance.app_server.subnet_id
 }
+output "instance_hostname" {
+  description = "Private DNS name of the EC2 instance."
+  value       = aws_instance.app_server.private_dns
+}
+
+output "instance_security_group_ids" {
+  value = aws_instance.app_server.vpc_security_group_ids
+}
+
+output "instance_subnet" {
+  value = aws_instance.app_server.subnet_id
+}
+
++ output "instance_ami" {
++   description = "AMI used to create the EC2 instance."
++   value       = aws_instance.app_server.ami
++ }
